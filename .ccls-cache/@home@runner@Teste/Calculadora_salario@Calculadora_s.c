7ba573ff4e9c2e4f7e864457da83e4a50448d6fa
FelipeGoncalvesAlmeida <160501854+FelipeGoncalvesAlmeida@users.noreply.github.com>
2024-03-29 15:25:03 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Teste/Calculadora_salario@Calculadora_s.c
@@ -0,0 +1,35 @@
+// Cabeçalho
+// Autor: Felipe Gonçalves de Almeida
+// Objetivo: Calcular o salário de um funcionário
+// Data: 28/03/2024
+
+#include <stdio.h>
+
+int main(void) {
+
+
+int ano_contrataçao, ano_atual, salario_base, porc_bonus ;
+  const float porc_bonus = 0.05
+
+  printf("Digite o ano de contratação: ");
+  scanf("%d",& ano_contrataçao);
+
+  printf("Digite o ano atual: ");
+  scanf("%d", &ano_atual);
+
+  printf("Digite o salário base: ");
+  scanf("%d", &salario_base);
+
+  printf( "Digite a porcentagem de bônus: ");
+
+  
+  
+  
+
+
+
+
+
+
+  
+}
